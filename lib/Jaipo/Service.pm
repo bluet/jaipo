82f3ef412e5747c32027ab101570bd00ad529787
c9s <cornelius.howl+git@gmail.com>
2009-02-08 01:37:13 +0800

    - provide prereq_plugins method for service plugins

--- lib/Jaipo/Service.pm
+++ lib/Jaipo/Service.pm
@@ -3,10 +3,22 @@ use warnings;
 use strict;
 use base qw/Class::Accessor::Fast/;
 
+
+
+=head1 FUNCTIONS
+
+=head2 new
+
+=cut
+
 sub new {
 	my $class = shift;
 	return bless {} , $class;
 }
+
+=head2 init
+
+=cut
 
 sub init {
 	my $self = shift;
@@ -23,9 +35,18 @@ called right before every request
 sub new_request {
 	my $self = shift;
 
-
 }
 
+
+=head2 prereq
+
+Returns an array of plugin module names that this plugin depends on.
+
+=cut 
+
+sub prereq_plugins {
+	return ();
+}
 
 1;
 

e9290c5a6b36e2f27e7a18d086ac2a0376894646
c9s <cornelius.howl+git@gmail.com>
2009-02-08 22:48:20 +0800

    - twitter init function, let service plugin asks    caller to setup user parameters

--- lib/Jaipo/Service/Twitter.pm
+++ lib/Jaipo/Service/Twitter.pm
@@ -4,16 +4,26 @@ use strict;
 use Net::Twitter;
 use base qw/Jaipo::Service Class::Accessor::Fast/;
 
-__PACKAGE__->mk_accessors (qw/core/);
 
 sub init {
-	my $self = shift;
-	my %opt = @_;
-	my $twitter = Net::Twitter->new( %opt );
+	my $self   = shift;
+	my $caller = shift;
+	my $opt = $self->options;
 
+	unless( $opt->{username} and $opt->{password} ) {
+
+		# request to setup parameter
+		$caller->setup_service ( {
+				package_name => __PACKAGE__,
+				require_args => {
+					username => { label => 'Username', description => '' },
+					password => { label => 'Password', description => '' },
+		} } , $opt );
+	}
+
+	my $twitter = Net::Twitter->new( %$opt );
 	$self->core( $twitter );
 }
-
 
 
 1;

e24fa65f3f420c9f60279965d59ebdc4dde9ab1a
c9s <cornelius.howl+git@gmail.com>
2009-02-16 18:00:15 +0800

    - let twitter print message with table

--- lib/Jaipo/Service/Twitter.pm
+++ lib/Jaipo/Service/Twitter.pm
@@ -3,6 +3,8 @@ use warnings;
 use strict;
 use Net::Twitter;
 use base qw/Jaipo::Service Class::Accessor::Fast/;
+use Text::Table;
+use Lingua::ZH::Wrap 'wrap';
 use utf8;
 
 sub init {
@@ -62,9 +64,27 @@ sub init {
 sub get_table {
 	my $self = shift;
 	my @cols = @_;
-	use Text::Table;
 	my $tb = Text::Table->new( @cols );
 	return $tb;
+}
+
+
+sub layout_message {
+	my $self = shift;
+	my $lines = shift;
+	local $Lingua::ZH::Wrap::columns = 30;
+	local $Lingua::ZH::Wrap::overflow = 1;
+
+	# force Message column to be 60 chars width or more.
+	my $tb = $self->get_table( qw|User Source Message| );
+
+    for ( @$lines ) {
+		my $source = $_->{source};
+		# $source =~ s{<a href="(.*?)">(.*?)</a>}{$2 ($1)};
+		$source =~ s{<a href="(.*?)">(.*?)</a>}{$2};
+		$tb->add( $_->{user}->{name} , $source , wrap( $_->{text} )  );
+    }
+	return $tb->table . "";
 }
 
 
@@ -75,53 +95,27 @@ sub get_table {
 sub send_msg {
 	my ( $self , $message ) = @_;
 	my $result = $self->core->update({ status => $message });
-
 }
 
 # updates from user himself
 sub read_user_timeline {
     my $self = shift;
     my $lines = $self->core->user_timeline;  # XXX: give args to this method
-
-	use Lingua::ZH::Wrap 'wrap';
-
-	local $Lingua::ZH::Wrap::columns = 30;
-	local $Lingua::ZH::Wrap::overflow = 1;
-
-	# force Message column to be 60 chars width or more.
-
-	my $tb = $self->get_table( qw|User Message Source| );
-    for ( @$lines ) {
-		$tb->add( $_->{user}->{name} , wrap( $_->{text} ) , $_->{source} );
-    }
-	Jaipo->logger->info( "" . $tb->table );
+	Jaipo->logger->info( $self->layout_message( $lines ) );
 }
 
 # updates from user's friends or channel
 sub read_public_timeline {
     my $self = shift;
-
     my $lines = $self->core->friends_timeline;  # XXX: give args to this method
-
-	my $tb = $self->get_table( qw|User Message Source| );
-    for ( @$lines ) {
-		$tb->add( $_->{user}->{name} , $_->{text} , $_->{source} );
-    }
-	Jaipo->logger->info( $tb->table );
-
+	Jaipo->logger->info( $self->layout_message( $lines ) );
 }
 
 
 sub read_global_timeline {
     my $self = shift;
-
     my $lines = $self->core->public_timeline;  # XXX: give args to this method
-
-	my $tb = $self->get_table( qw|User Message Source| );
-    for ( @$lines ) {
-		$tb->add( $_->{user}->{name} , $_->{text} , $_->{source} );
-    }
-	Jaipo->logger->info( $tb->table );
+	Jaipo->logger->info( $self->layout_message( $lines ) );
 }
 
 

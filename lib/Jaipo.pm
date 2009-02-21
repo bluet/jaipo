d9d1f9c2ca27f5d521dce11478126017f5dd3812
c9s <cornelius.howl+git@gmail.com>
2009-02-21 23:57:58 +0800

    - update MANIFEST

--- lib/Jaipo.pm
+++ lib/Jaipo.pm
@@ -17,11 +17,11 @@ Jaipo - Jaiku (and other micro-blogging sites) Client
 
 =head1 VERSION
 
-Version 0.01
+Version 0.03
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.03';
 
 =head1 DESCRIPTION
 
